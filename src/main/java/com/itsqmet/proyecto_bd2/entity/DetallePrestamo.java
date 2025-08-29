78001546d48f6281fc32bed3353a03cdc59a7cc1
chasi123 <christopherchasiloa56@gmail.com>
2025-08-28 19:10:51 -0500

    Echo cardinalidad y validaciones

--- src/main/java/com/itsqmet/proyecto_bd2/entity/DetallePrestamo.java
+++ src/main/java/com/itsqmet/proyecto_bd2/entity/DetallePrestamo.java
@@ -1,11 +1,11 @@
 package com.itsqmet.proyecto_bd2.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -15,7 +15,19 @@ public class DetallePrestamo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     //cantidad
+    @NotNull(message = "La cantidad es obligatoria")
+    @Min(value = 1, message = "La cantidad debe ser al menos 1")
     private Integer cantidad;
     //total
+    @NotNull(message = "El total es obligatorio")
+    @Min(value = 1, message = "El total debe ser mayor a 0")
     private Double total;
+
+    @ManyToOne
+    @JoinColumn(name = "prestamo_id")
+    private Prestamo prestamo;
+
+    @OneToMany(mappedBy = "detallePrestamo")
+    private List<Videojuego> videojuegos;
+
 }
--- src/main/java/com/itsqmet/proyecto_bd2/entity/Videojuego.java
+++ src/main/java/com/itsqmet/proyecto_bd2/entity/Videojuego.java
@@ -1,9 +1,7 @@
 package com.itsqmet.proyecto_bd2.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,14 +13,26 @@ public class Videojuego {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     //titulo
+    @NotBlank(message = "El título es obligatorio")
+    @Size(min = 2, max = 100, message = "El título debe tener entre 2 y 100 caracteres")
     private String titulo;
     //genero
+    @NotBlank(message = "El Genero es obligatorio")
     private String genero;
     //stock
+    @NotNull(message = "El stock es obligatorio")
+    @Min(value = 0, message = "El stock no puede ser negativo")
     private Integer stock;
     //plataforma
+    @NotNull(message = "Plataforma de ser obligatoria")
     private String plataforma;
     //precio Unitario
+    @NotNull(message = "El precio es obligatorio")
+    @Min(value = 1, message = "El precio debe ser mayor a 0")
     private Double precio;
+
+    @ManyToOne
+    @JoinColumn(name = "detalle_prestamo_id")
+    private DetallePrestamo detallePrestamo;
 
 }
